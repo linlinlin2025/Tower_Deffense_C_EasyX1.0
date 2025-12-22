9c838f4d72b3f200d53e41593e93a5f3d5ef579a
si-nana <18070394295@163.com>
2025-12-22 10:41:09 +0800

    enemy.h

--- enemy-同学c/game_enemy.h
+++ enemy-同学c/game_enemy.h
@@ -3,6 +3,8 @@
 #include <graphics.h>		
 #include <conio.h>
 #include <stdio.h>
+#include <cstdlib>  // 包含 rand() 和 srand()
+#include <ctime>    // 包含 time()
 using namespace std;
 struct enemy {
 	int enemy_radius;//敌人初始位置
@@ -21,9 +23,10 @@ struct enemy {
 };
 //extern int enemy_x, enemy_y;         /*待定，不清楚要不要用*/
 //extern int die_x, die_y;//死亡位置   /*待定，不清楚要不要用*/
-int Deadenemy_money(int die_num);//死亡敌人返回的总钱数
-int Deadenemy_count(int ifshoot);//死亡敌人数
 int Point_x_y(void);//返回给B敌人的位置坐标,会不断更新位置
+int Deadenemy_count(int ifshoot);//死亡敌人数给D
+int Deadenemy_level(int point);//D给我当前积分数，我判断后变更等级，返回死亡敌人等级数给D
+int Deadenemy_money(int die_num);//死亡敌人返回的总钱数给A
 int Reach_basement_num(int base_x,int base_y);//接受基地坐标,返回到达基地的敌人个数给D
 int Enemy_route_design(int enemy_start_x, int enemy_start_y);//接受敌人初始位置,敌人路线设计（碰到棕色块往其他地方走）
 int Accelerate_speedandlevel();//根据积分数改变敌人速度和等级
